5d568c7e353a21955d2273153af6d742581dea67
Apurbu Sarder <apurbusarderjust321@gmail.com>
2021-03-01 03:25:59 +0600

    Saving Orders to Database

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Main from './Components/main';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store'
+
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
             <Main />
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
+
     </div>
   );
 }
